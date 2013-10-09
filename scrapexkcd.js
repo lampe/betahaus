7de5b8a612e2090cd435c1acf3ddf6c6d8458356
Michael Lazarski <michael@lazarski.me>
2013-10-09 21:13:59 +0200

    lets turn it into a module

--- scrapexkcd.js
+++ scrapexkcd.js
@@ -6,13 +6,16 @@ var url = 'http://xkcd.com/';
 img = '';
 title = '';
 function start() {
+	//lets get the image of today 
 	requestImageFromXkcd();
+	//send it to the user
 	http.createServer(function (req, res) {
 		res.writeHead(200, {'Content-Type': 'text/html'});
 		res.end('<img border="0" src="'+img+'"> <p>'+title+'');
 	}).listen(1337, '127.0.0.1');
 }
 function requestImageFromXkcd(){
+// let the request module get the url and print it into the console and save it
 request(url, function(err, resp, body) {
     if (err)
         throw err;
